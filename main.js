b702213be89f5241c1c9d6ba950753488e0a1914
Bartek Świder <bswiincent@gmail.com>
2020-04-22 14:08:52 +0200

    add meeting function

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ const name = 'Bartek Świder';
 const age = 29;
 
 console.log(`Witaj w consoli na tej stronie ja nazywam się ${name} i mam ${age} lat. Miło cię poznać`);
+
+function welcome(name, lastName) {
+    const name1 = name;
+    const lastName1 = lastName;
+
+    alert (`Witaj ${name1} ${lastName1} na mojej stronie`);
+}
+
+welcome(prompt("Podaj swoje imię"), prompt("Podaj swoje Nazwisko"));
